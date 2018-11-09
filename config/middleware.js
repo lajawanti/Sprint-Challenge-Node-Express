8c1f2f5915c1543e43ab8e4afc9e8a682fd510d7
Lajawanti <lajawanti.dhake@gmail.com>
2018-11-09 14:12:07 -0500

    Projects and Actions POST Done

--- config/middleware.js
+++ config/middleware.js
@@ -5,6 +5,7 @@ const cors = require('cors');
 
 module.exports = server => {
     server.use(express.json()); 
+    
     server.use(morgan('short')); 
     server.use(cors());
 };
--- index.js
+++ index.js
@@ -5,9 +5,10 @@ const actiondb = require('./data/helpers/actionModel.js');
 const projectdb = require('./data/helpers/projectModel.js');
 
 //config-- Middleware
-const configureMiddleware = require('./config/middleware.js');
-
+//const configureMiddleware = require('./config/middleware.js');
 const server = express();
+server.use(express.json()); // built in
+
 /*****  PROJECTS and ACTIONS  GET *****/
 server.get('/api/projects', (request, response) => {
     projectdb.get()
@@ -39,6 +40,37 @@ server.get('/api/actions/:id', (request, response) => {
                 response.status(500).json({error : 'The actions data could not be retrieved'})
            }) 
 })
+
+/*****  PROJECTS and ACTIONS  POST *****/
+server.post('/api/projects', (request, response) => {
+    //const {name, description, completed} = request.body;
+    if(request.body.name !== undefined && request.body.description !== undefined && request.body.completed !== undefined) {
+    projectdb.insert(request.body)
+          .then(projectId => {
+                response.status(201).json(projectId);
+           })
+          .catch(error => {
+                response.status(500).json({message : 'error creating project', error});
+           }) 
+    } else {
+        response.status(400).json({ errorMessage: "Please provide name and description for the project." })
+    }
+}) 
+
+server.post('/api/actions', (request, response) => {
+    const {project_id, description, notes, completed} = request.body;
+    if(project_id !== undefined && notes !== undefined && description !== undefined && completed !== undefined) {
+    actiondb.insert(request.body)
+          .then(actionId => {
+                response.status(201).json(actionId);
+           })
+          .catch(error => {
+                response.status(500).json({message : 'error creating action', error});
+           }) 
+    } else {
+        response.status(400).json({ errorMessage: "Please provide notes and description for the action." })
+    }
+}) 
 
 
 
